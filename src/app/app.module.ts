593dd67903f5aff620e9c132df88d9d7fea44d85
Marcelrp <marcel.pie@hotmail.com>
2024-01-18 11:51:17 -0300

    criando os components, services, interface, pipe etc!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CoursesComponent } from './courses/components/courses/courses.component';
+import { ErrorDialogComponent } from './courses/components/error-dialog/error-dialog.component';
+import { CourseFormComponent } from './courses/components/course-form/course-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    ErrorDialogComponent,
+    CourseFormComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
