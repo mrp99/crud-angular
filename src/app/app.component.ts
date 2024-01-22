b5c5cfb3436dd94f3967ee9b26c977fa7312def5
Marcelrp <marcel.pie@hotmail.com>
2024-01-22 14:01:33 -0300

    Atualizaçao para versão 14!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crud-angular';
+
+  public title: string = 'CRUD Angular + Spring';
 }
